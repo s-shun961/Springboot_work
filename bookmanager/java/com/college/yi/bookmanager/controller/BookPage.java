168df783fe7cf85d9cc67993eb38a0dd37192f2b
s-shun961 <sakai_shunji@y-i-group.co.jp>
2025-04-27 23:28:29 +0900

    Screendisplay
    画面上に書籍リストを表示するコードを追加

---
+++ bookmanager/java/com/college/yi/bookmanager/controller/BookPage.java
@@ -0,0 +1,30 @@
+package com.college.yi.bookmanager.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.college.yi.bookmanager.model.Book;
+import com.college.yi.bookmanager.service.BookService;
+
+@Controller
+@RequestMapping("/books")
+public class BookPage {
+	private final BookService bookService;
+	
+	public BookPage(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+@GetMapping
+public String booksPage(Model model) {
+	List<Book> bookList = bookService.findAll();
+	model.addAttribute("bookList",bookList);
+	// index.htmlを表示
+	return "index";
+	}
+
+}
